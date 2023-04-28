47a085fc5b1c101741c1b131a20cd035c36151d4
br058188 <br058188@intelbras.com.br>
2023-04-27 21:44:10 -0300

    remote default host change

--- general/configs.py
+++ general/configs.py
@@ -1,4 +1,4 @@
-default_client_host = 'localhost'
+default_client_host = '150.162.244.227'
 default_client_port = 8080
-default_server_host = 'localhost'
+default_server_host = '150.162.244.227'
 default_server_port = 8080
